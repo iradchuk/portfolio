8598ce1e1f5a5787de92c3fafb42dc49de27ba49
Ihor Radchuk <radchuk2808@gmail.com>
2024-01-05 23:48:17 +0200

    add age calculator

---
+++ Notes/script.js
@@ -0,0 +1,43 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.btn');
+
+function showNotes() {
+   notesContainer.innerHTML = localStorage.getItem('data');
+}
+showNotes();
+
+function updateStorage() {
+    localStorage.setItem('data', notesContainer.innerHTML);
+}
+
+
+createBtn.addEventListener('click', ()=> {
+    let inputBox = document.createElement('p');
+    let img = document.createElement('img');
+    inputBox.className = 'input-box';
+    inputBox.setAttribute('contenteditable', 'true');
+    img.src = 'img/delete.png';
+    notesContainer.appendChild(inputBox).appendChild(img);
+    updateStorage();
+})
+
+notesContainer.addEventListener('click', (e)=>{
+    if (e.target.tagName === 'IMG') {
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if (e.target.tagName === 'P') {
+        let notes = document.querySelectorAll('.input-box');
+        notes.forEach(nt => {
+            nt.onkeyup = function () {
+                updateStorage();
+            }
+        })
+    }
+})
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'Enter') {
+        document.execCommand('insertLineBreak');
+        event.preventDefault();
+    }
+})
